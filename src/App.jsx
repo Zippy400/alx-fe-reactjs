a828761af2ba856ae08d19f49090a87e6f9362b5
Zipporah Mutinda <mbinyazipporah22@gmail.com>
2025-07-29 20:39:50 +0300

    Initial setup for GitHub User Search App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '2rem' }}>
+      <h1>GitHub User Search</h1>
+    </div>
+  );
+}
+
+export default App;
