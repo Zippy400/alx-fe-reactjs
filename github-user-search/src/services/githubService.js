eee10c886fac371fc3d684b34aa220a7c1cc5dbe
Zippy400 <mbinyazipporah22@gmail.com>
2025-08-07 14:43:55 +0300

    added https, intead of http in githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const fetchUserData = async (username) => {
-    const response = await axios.get(`http://api.github.com/users/${username}`);
+    const response = await axios.get(`https://api.github.com/users/${username}`);
     return response.data;
 }
