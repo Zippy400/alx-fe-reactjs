3bd6100e354adbe2cd7110a6e7325a9d43613d38
Zippy400 <mbinyazipporah22@gmail.com>
2025-08-07 14:38:28 +0300

    Add GitHub API integration and basic search feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="p-4 text-center">
       <h1 className="text-3xl font-bold mb-4">GitHub User Search</h1>
       {/* More content coming in future tasks */}
+      <Search/>
     </div>
   );
 }
---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { fetchUserData } from "../services/githubService";
+
+function Search() {
+    const [username, setUsername] = useState('');
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError('');
+        setUserData(null);
+
+        try {
+            const data = await fetchUserData(username);
+            setUserData(data);
+        } catch (error) {
+            setError('We cannot find the user');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return(
+        <div className="">
+            <form onSubmit={handleSubmit} action="">
+                <input type="text" placeholder="Enter github username" value={username} onChange={(e) => setUsername(e.target.value)} id="" />
+                <button type="submit">Search</button>
+            </form>
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            {userData && (
+                <div>
+                    <img src={userData.avatar_url} alt="Avatar" width={100} />
+                    <h2>{userData.name || userData.login}</h2>
+                    <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
+                        View GitHub Profile
+                    </a>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Search;
