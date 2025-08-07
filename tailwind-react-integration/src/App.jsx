6291c5326873f2c5fd5af4e5b7d3b65d536c5499
Zippy400 <mbinyazipporah22@gmail.com>
2025-08-07 17:17:55 +0300

    Initialized project with tailwind.css

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      </div>
+      <div className="card">
+        <h1 className="text-3xl font-bold text-blue-800">
+          Tailwind CSS is working!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
