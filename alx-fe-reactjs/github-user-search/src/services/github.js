8b35a0bb8cb60851de2e9c77c2ca3e0a96b46332
Wilson Kilonzo <mutidawilz@gmail.com>
2025-08-07 13:44:37 +0300

    Correct arrangement

--- alx-fe-reactjs/github-user-search/src/services/github.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = import.meta.env.VITE_APP_GITHUB_API_URL;
-
-export const searchUsers = async (query) => {
-  const response = await axios.get(`${BASE_URL}/search/users?q=${query}`);
-  return response.data;
-};
